cea99b9396ceb05b8ceb25522f7a414870333d66
Brian Haubrick <bhaubrick@telus.net>
2020-08-10 17:03:22 -0700

    adding sprint-2 and 3 folders to project

--- sprint-1/src/components/Banner.js
+++ sprint-1/src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = (props) => {
   return (
-    <div className='banner flex-wrapper container'>
+    <div className='banner container'>
       <div className='video__container'>
         <section className='video__player' controls>
           <img src={props.mainVideo.image} />
