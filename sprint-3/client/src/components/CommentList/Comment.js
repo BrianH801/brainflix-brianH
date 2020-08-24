f3218497460d88b16e75088a62f6160fcc2d2128
Brian Haubrick <bhaubrick@telus.net>
2020-08-23 19:47:07 -0700

    Still working on styling

--- sprint-3/client/src/components/CommentList/Comment.js
+++ sprint-3/client/src/components/CommentList/Comment.js
@@ -3,18 +3,20 @@ import React from 'react';
 function Comment({ id, date, name, comment }) {
   return (
     <li className='comment__id' id={id}>
-      <div className='comment__name-date'>
-        <div className='comment__name'>
-          <h2>{name}</h2>
+      <div className='comment__name-comment'>
+        <div className='comment__name-date'>
+          <div className='comment__name'>
+            <h4>{name}</h4>
+          </div>
+          <div className='comment__date'>
+            <p>{date}</p>
+          </div>
         </div>
-        <div className='comment__date'>
-          <p>{date}</p>
-        </div>
-      </div>
-      <div className='comment__avatar-comment'>
-        <div className='comment__avatar'></div>
-        <div className='comment__comment'>
-          <p>{comment}</p>
+        <div className='comment__avatar-comment'>
+          <div className='comment__avatar'></div>
+          <div className='comment__comment'>
+            <p>{comment}</p>
+          </div>
         </div>
       </div>
     </li>
